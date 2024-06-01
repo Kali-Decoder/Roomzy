7a29f4bd680941c47c5260b5c8b48f6583aa9a6e
Snehagupta1907 <snehagupta98930@gmail.com>
2024-06-02 02:46:57 +0530

    prefrence controller fix

--- server/controllers/prefrenceController.js
+++ server/controllers/prefrenceController.js
@@ -1,9 +1,16 @@
 import Preference from "../model/preference.model.js";
 import { ErrorHandler } from "../utils/errorHandler.js";
+import User from "../model/user.model.js";
 
 export const setPreferences = async (req, res, next) => {
+ 
     try {
+      // console.log(req.user)
       const preference = await Preference.create({ ...req.body, user_id: req.user.id });
+
+      const user = await User.findById(req.user.id);
+      user.preferences.push(preference._id);
+      await user.save();
   
       res.status(201).json({
         success: true,
