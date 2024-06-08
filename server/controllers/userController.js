94a241a1320f0227092daae4b6c8b58266143ab0
Rkesh Roushan <rakeshroushan2341@gmail.com>
2024-06-08 22:31:26 +0530

    added referral system in backend

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,6 @@
-import User from '../model/user.model.js';
-import sendTokenResponse from '../utils/jwtToken.js';
-import { ErrorHandler } from '../utils/errorHandler.js';
+import User from "../model/user.model.js";
+import sendTokenResponse from "../utils/jwtToken.js";
+import { ErrorHandler } from "../utils/errorHandler.js";
 
 export const registerUser = async (req, res, next) => {
   const {
@@ -15,11 +15,13 @@ export const registerUser = async (req, res, next) => {
     profile_picture_url,
   } = req.body;
 
+  const referralUserId = req.query.referral;
+  console.log(referralUserId);
   try {
     const userExists = await User.findOne({ email });
 
     if (userExists) {
-      return next(new ErrorHandler(400, 'User already exists with this email'));
+      return next(new ErrorHandler(400, "User already exists with this email"));
     }
 
     const user = await User.create({
@@ -34,6 +36,12 @@ export const registerUser = async (req, res, next) => {
       profile_picture_url,
     });
 
+    if (referralUserId) {
+      const referrer = await User.findById(referralUserId);
+      console.log(referrer);
+      referrer.rewards += 10;
+      await referrer.save();
+    }
     sendTokenResponse(user, 201, res);
   } catch (error) {
     next(error);
@@ -41,75 +49,76 @@ export const registerUser = async (req, res, next) => {
 };
 
 export const loginUser = async (req, res, next) => {
-    const { email, password } = req.body;
-  
-    // Check if email and password were provided
-    if (!email || !password) {
-      return next(new ErrorHandler(400, 'Please provide email and password'));
-    }
-  
-    try {
-      // Find user by email
-      const user = await User.findOne({ email }).select('+password_hash');
-  
-      if (!user) {
-        return next(new ErrorHandler(401, 'Invalid email or password'));
-      }
-  
-      // Check if password matches
-      const isMatch = await user.matchPassword(password);
-  
-      if (!isMatch) {
-        return next(new ErrorHandler(401, 'Invalid email or password'));
-      }
-  
-      // If matched, send token response
-      sendTokenResponse(user, 200, res);
-    } catch (error) {
-      next(error);
-    }
-  };
+  const { email, password } = req.body;
 
-  export const logoutUser = async (req, res, next) => {
-    try {
-      res.clearCookie("token");
-  
-      res
-        .status(200)
-        .json({ success: true, message: "User logged out successfully" });
-    } catch (error) {
-      next(error);
-    }
-  };
-
-
-export const changePassword = async (req, res, next) => {
-    const { currentPassword, newPassword, confirmNewPassword } = req.body;
-
-  if (!currentPassword || !newPassword || !confirmNewPassword) {
-    return next(new ErrorHandler(400, 'Please provide current and new password'));
+  // Check if email and password were provided
+  if (!email || !password) {
+    return next(new ErrorHandler(400, "Please provide email and password"));
   }
 
   try {
-    const user = await User.findById(req.user.id).select('+password_hash');
+    // Find user by email
+    const user = await User.findOne({ email }).select("+password_hash");
 
     if (!user) {
-      return next(new ErrorHandler(404, 'User not found'));
+      return next(new ErrorHandler(401, "Invalid email or password"));
+    }
+
+    // Check if password matches
+    const isMatch = await user.matchPassword(password);
+
+    if (!isMatch) {
+      return next(new ErrorHandler(401, "Invalid email or password"));
+    }
+
+    // If matched, send token response
+    sendTokenResponse(user, 200, res);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const logoutUser = async (req, res, next) => {
+  try {
+    res.clearCookie("token");
+
+    res
+      .status(200)
+      .json({ success: true, message: "User logged out successfully" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const changePassword = async (req, res, next) => {
+  const { currentPassword, newPassword, confirmNewPassword } = req.body;
+
+  if (!currentPassword || !newPassword || !confirmNewPassword) {
+    return next(
+      new ErrorHandler(400, "Please provide current and new password")
+    );
+  }
+
+  try {
+    const user = await User.findById(req.user.id).select("+password_hash");
+
+    if (!user) {
+      return next(new ErrorHandler(404, "User not found"));
     }
 
     // Check if current password matches
     const isMatch = await user.matchPassword(currentPassword);
 
     if (!isMatch) {
-      return next(new ErrorHandler(401, 'Current password is incorrect'));
+      return next(new ErrorHandler(401, "Current password is incorrect"));
     }
 
     if (newPassword !== confirmNewPassword) {
-        throw new ErrorHandler(
-          400,
-          "New password and confirm password do not match"
-        );
-      }
+      throw new ErrorHandler(
+        400,
+        "New password and confirm password do not match"
+      );
+    }
 
     // Update password
     user.password_hash = newPassword;
@@ -124,44 +133,38 @@ export const changePassword = async (req, res, next) => {
 };
 
 export const getUserProfile = async (req, res, next) => {
-    try {
-      const user = await User.findById(req.user.id).populate('preferences');
-      
-      if (!user) {
-        return next(new ErrorHandler(404, 'User not found'));
-      }
-  
-      res.status(200).json({
-        success: true,
-        data: user
-      });
-    } catch (error) {
-      next(error);
+  try {
+    const user = await User.findById(req.user.id).populate("preferences");
+
+    if (!user) {
+      return next(new ErrorHandler(404, "User not found"));
     }
-  };
-  
 
-  export const updateUserProfile = async (req, res, next) => {
-    try {
-      const user = await User.findByIdAndUpdate(req.user.id, req.body, {
-        new: true,
-        runValidators: true
-      });
-  
-      if (!user) {
-        return next(new ErrorHandler(404, 'User not found'));
-      }
-  
-      res.status(200).json({
-        success: true,
-        data: user
-      });
-    } catch (error) {
-      next(error);
+    res.status(200).json({
+      success: true,
+      data: user,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateUserProfile = async (req, res, next) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.user.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!user) {
+      return next(new ErrorHandler(404, "User not found"));
     }
-  };
 
-
-
-
-
+    res.status(200).json({
+      success: true,
+      data: user,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
--- server/model/user.model.js
+++ server/model/user.model.js
@@ -26,6 +26,7 @@ const userSchema = new mongoose.Schema({
   profile_picture_url: { type: String },
   bio: { type: String },
   preferences: { type: mongoose.Schema.Types.ObjectId, ref: 'Preference' },
+  rewards: { type: Number, default: 0 },
   created_at: { type: Date, default: Date.now },
 });
 
