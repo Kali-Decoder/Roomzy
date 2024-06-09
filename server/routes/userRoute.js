fa29e5ee5c37ca7fbaf2fd6bae2dce7277ce22da
Snehagupta1907 <snehagupta98930@gmail.com>
2024-06-09 09:06:07 +0530

    get all user

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { registerUser,loginUser,logoutUser,changePassword,getUserProfile,updateUserProfile, getUsernameByUserId, getRewardByUsername, getIdByUsername} from "../controllers/userController.js";
+import { registerUser,loginUser,logoutUser,changePassword,getUserProfile,updateUserProfile, getUsernameByUserId, getRewardByUsername, getIdByUsername, getAllUsers} from "../controllers/userController.js";
 import { setPreferences,getPreferences } from "../controllers/prefrenceController.js";
 import { auth } from "../middlewares/auth.js";
 
@@ -16,5 +16,6 @@ router.get('/preferences', auth, getPreferences);
 router.get('/:userId/username', auth, getUsernameByUserId);
 router.get('/:username/rewards',  getRewardByUsername);
 router.get('/:username/wallet', getIdByUsername);
+router.get('/',auth,getAllUsers)
 
 export default router;
