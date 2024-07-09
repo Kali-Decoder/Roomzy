f74ce4605093c7e39115e74f8315cc3b45ba5db0
Rkesh Roushan <rakeshroushan2341@gmail.com>
2024-07-10 02:17:46 +0530

    changes to correctly verify

--- client/src/utils/verifyTelegramHash.js
+++ client/src/utils/verifyTelegramHash.js
@@ -1,28 +1,23 @@
 import CryptoJS from "crypto-js";
 
 export const verifyTelegramHash = (data, botToken) => {
-  const { id, first_name, last_name, username, auth_date, hash } = data;
-  const secret = CryptoJS.HmacSHA256(
-    auth_date + "\n" + id,
-    botToken
-  ).toString();
-  const hmac = CryptoJS.HmacSHA256(
-    auth_date +
-      "\n" +
-      id +
-      "\n" +
-      first_name +
-      "\n" +
-      last_name +
-      "\n" +
-      username +
-      "\n" +
-      botToken,
-    secret
-    ).toString(CryptoJS.enc.Hex);
-    
-    console.log("HMAC", hmac);
-    console.log("HASH", hash);
+  const { hash, ...params } = data;
+
+  // Sort the parameters alphabetically by key
+  const sortedKeys = Object.keys(params).sort();
+  const sortedParams = sortedKeys
+    .map((key) => `${key}=${params[key]}`)
+    .join("\n");
+
+  // Generate the HMAC-SHA256 hash
+  const secretKey = CryptoJS.SHA256(botToken);
+  const hmac = CryptoJS.HmacSHA256(sortedParams, secretKey).toString(
+    CryptoJS.enc.Hex
+  );
+
+  console.log("Sorted Parameters:", sortedParams);
+  console.log("HMAC:", hmac);
+  console.log("Hash:", hash);
 
   return hmac === hash;
 };
